7bb6f6ec3a410e236f3bba09814d1975325175c0
tamcos <166866375+tamcos@users.noreply.github.com>
2024-04-17 21:01:46 +0200

    Some overall improvements

--- years/23/day_01/main.ts
+++ years/23/day_01/main.ts
@@ -1,52 +1,56 @@
 #!/usr/bin/env -S deno run --allow-read
 import { takeFirstLast } from 'tools/take_first_last.ts';
-import { assertExistsExpr } from 'tools/assert_exists_expr.ts';
 import { isDefined } from 'tools/is_defined.ts';
 
-const input = await Deno.readTextFile('input');
-
-const part1 = getCalibrationValuesSum(input, parseInputPart1);
-console.info('Part 1', part1);
-
-const part2 = getCalibrationValuesSum(input, parseInputPart2);
-console.info('Part 2', part2);
-
-function parseInputPart1(input: string) {
-	const allDigitsRe = /\d/g;
-
-	return input.split('\n').map((l) => assertExistsExpr(l.match(allDigitsRe)));
+// Things for the first part
+function getAllDigits(string: string) {
+	return (string.match(/\d/g) ?? []).filter(isDefined).map(Number);
 }
 
-function parseInputPart2(input: string) {
-	const allDigitsRe = /\d|one|two|three|four|five|six|seven|eight|nine/g;
-
-	const spelledToValue = Object.fromEntries(
-		allDigitsRe.source.split('|').slice(1).map((d, i) => [d, 1 + i]),
+// Things for the second part
+class SpelledDigits {
+	static spelledDigitRe = /one|two|three|four|five|six|seven|eight|nine/;
+	// Like {one: 1, two: 2, ..., nine: 9}
+	static spelledDigitToNumber = Object.fromEntries(
+		this.spelledDigitRe.source.split('|').map((k, i) => [k, 1 + i]),
 	);
-	const allDigits: (string | number)[][] = [];
+}
 
-	for (const line of input.split('\n')) {
-		const digits: typeof allDigits[number] = [];
-
-		while (true) {
-			const match = allDigitsRe.exec(line);
-			if (!isDefined(match)) break;
-
-			const { 0: d, index } = match;
-			digits.push(spelledToValue[d] ?? d);
-			// Handle overlapping spelled numbers like 'oneight'
-			allDigitsRe.lastIndex = index + 1;
-		}
-		allDigits.push(digits);
+function getAllDigitsWithSpelled(string: string) {
+	const allDigitsRe = new RegExp(
+		// Extend to include spelled digits
+		`\d|${SpelledDigits.spelledDigitRe.source}`,
+		'g',
+	);
+	const allDigits: number[] = [];
+	while (true) {
+		// Get the first match and update `allDigitsRe` state
+		const match = allDigitsRe.exec(string);
+		if (!isDefined(match)) break;
+		const { 0: d, index } = match;
+		allDigits.push(SpelledDigits.spelledDigitToNumber[d] ?? Number(d));
+		// Handle overlapping spelled numbers like "oneight"
+		// by starting right after the previous match
+		allDigitsRe.lastIndex = index + 1;
 	}
 	return allDigits;
 }
 
-function getCalibrationValuesSum(
-	input: string,
-	parseInput: (input: string) => (string | number)[][],
-) {
-	return parseInput(input)
-		.map((ns) => Number(takeFirstLast(ns).join('')))
-		.reduce((a, b) => a + b, 0);
+// Thigns for both parts
+function getCalibrationValue(ds: number[]) {
+	return Number(takeFirstLast(ds).join(''));
 }
+// ---
+
+const input = await Deno.readTextFile('input');
+const inputLines = input.split('\n');
+
+const part1 = inputLines
+	.map((l) => getCalibrationValue(getAllDigits(l)))
+	.reduce((a, b) => a + b, 0);
+console.log('Part 1', part1);
+
+const part2 = inputLines
+	.map((l) => getCalibrationValue(getAllDigitsWithSpelled(l)))
+	.reduce((a, b) => a + b, 0);
+console.log('Part 2', part2);
